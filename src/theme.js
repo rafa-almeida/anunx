7620ba738c73f858b56efd4f8656fa85324bf499
Rafael Almeida <rafasegt@gmail.com>
2022-08-11 18:26:36 -0300

    adicionando Template Default, Header, User Dashboard

--- src/theme.js
+++ src/theme.js
@@ -3,7 +3,7 @@ import { createTheme } from '@material-ui/core/styles'
 const theme = createTheme({
     palette: {
         primary: {
-            main: '#000000',
+            main: '#2196f3',
         }
     }
 })
